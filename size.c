c15f2ee798580a4aa936dc43e853f937209040dc
kessa99 <kipre325@mail.com>
2022-09-19 01:40:06 -0700

    size

---
+++ size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - printt the size
+ *
+ * Return: 0 (Sucess)
+*/
+
+int main(void)
+{
+	  
+	printf("%lu\n", sizeof(int));
+	printf("%lu\n", sizeof(char));
+	printf("%lu\n", sizeof(float));
+	printf("%lu\n", sizeof(double));
+	printf("%lu\n", sizeof(long));
+
+	return (0);
+}
